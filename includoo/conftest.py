97de4a8558cc1b2c52e8e45ae6542ce056b8eaf0
Nithish Raghunandanan <nithish.raghunandanan@zooplus.com>
2020-09-19 12:32:17 +0200

    Initial Commit

---
+++ includoo/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from includoo.users.models import User
+from includoo.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
