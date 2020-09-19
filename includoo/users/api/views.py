1d99778630373be0987af10cf7b86d88cee76a20
Nithish Raghunandanan <nithish.raghunandanan@zooplus.com>
2020-09-19 23:11:35 +0200

    Add the user creation & listing end points

--- includoo/users/api/views.py
+++ includoo/users/api/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.decorators import action
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
@@ -10,9 +11,10 @@ from .serializers import UserSerializer
 User = get_user_model()
 
 
-class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericViewSet):
+class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericViewSet, CreateModelMixin):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [AllowAny]
     lookup_field = "username"
 
     def get_queryset(self, *args, **kwargs):
@@ -22,3 +24,12 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericV
     def me(self, request):
         serializer = UserSerializer(request.user, context={"request": request})
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+    def perform_create(self, serializer_class):
+        serializer_class.save()
+
+
+    def list(self, request, *args, **kwargs):
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True, context={"request": request})
+        return Response(serializer.data)
