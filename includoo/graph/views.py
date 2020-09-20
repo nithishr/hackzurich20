655dca864aaa9c6c451f583a769269249f64ebf9
Nithish Raghunandanan <nithish.raghunandanan@zooplus.com>
2020-09-20 03:55:18 +0200

    Add the API to update the graph edge weights

--- includoo/graph/views.py
+++ includoo/graph/views.py
@@ -2,6 +2,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Edge
+from includoo.users.models import User
 
 from .serializers import EdgeSerializer
 
@@ -13,3 +14,28 @@ class EdgeAPI(APIView):
         edges = Edge.objects.all()
         serializer = EdgeSerializer(edges, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        sender = request.data["sender"]
+        recipient = request.data["recipient"]
+        mails = request.data["mails"]
+
+        s_user = User.objects.filter(email=sender)
+        r_user = User.objects.filter(email=recipient)
+
+        weight = sum(mails)
+        e = Edge.objects.filter(user1=s_user[0], user2=r_user[0])
+        if e:
+            e.update(user1=s_user[0], user2=r_user[0], weight=weight)
+        else:
+            e = Edge.objects.filter(user2=s_user[0], user1=r_user[0])
+            if not e:
+                e.create(user1=s_user[0], user2=r_user[0], weight=weight)
+            else:
+                e.update(user2=s_user[0], user1=r_user[0], weight=weight)
+
+
+        return Response()
+
+
+
